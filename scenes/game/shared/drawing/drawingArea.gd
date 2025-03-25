38c7108f501d8b0f6094ef95b1178c0f27a47eac
Daniel Sacré <daniel.sacre@gmx.de>
2025-03-25 15:20:19 +0100

    CRITICAL BUG FIX D2JAM #1
    CHANGES:
    - removed unused particle system
    - added check of mouse position change to reduce the amount of points added to the curves
    - added method to convert Line2D to Texture2D and prune the Line2D

--- scenes/game/shared/drawing/drawingArea.gd
+++ scenes/game/shared/drawing/drawingArea.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+################################################################################
+#### PRIVATE MEMBER VARIABLES ##################################################
+################################################################################
 var _drawingAllowed : bool = true
+var _lastMousePosition : Vector2 = Vector2.ZERO
 
+################################################################################
+#### EXPORT MEMBER VARIABLES ###################################################
+################################################################################
 @export var _pointsMaximum : int = 30
 
 ################################################################################
@@ -9,18 +16,35 @@ var _drawingAllowed : bool = true
 ################################################################################
 @onready var _lineVisual : Line2D = $playerFeedback/lineVisualizer/Line2D
 @onready var _lineTexture : Line2D = $mattePainting/lineVisualizer/Line2D
-@onready var _cloudEmitter : = $playerFeedback/GPUParticles2D
 @onready var _brush : Node2D = $playerFeedback/brush
+
+@onready var _mattePainting : SubViewport = $mattePainting
+@onready var _mattePersistence : Sprite2D = $mattePainting/mattePersistent
 
 ################################################################################
 #### PRIVATE MEMBER FUNCTIONS ##################################################
 ################################################################################
-func _manage_line(line : Line2D, newPoint : Vector2, remove : bool = true) -> void:
-	line.add_point(newPoint)
-
+func _manage_line(line : Line2D, newPoint : Vector2, lastPoint : Vector2, remove : bool = true) -> void:
 	if remove:
+		line.add_point(newPoint)
+
 		if line.points.size() > self._pointsMaximum:
 			line.remove_point(0)
+
+	else:
+		if newPoint != lastPoint:
+			line.add_point(newPoint)
+
+func _save_to_texture_and_prune() -> Texture2D:
+	# DESCRIPTION: Acquire the current display of the viewport and store it into
+	# a static image texture
+	var _tmp_image : Image = self._mattePainting.get_texture().get_image()
+	var _tmp_texture : ImageTexture = ImageTexture.create_from_image(_tmp_image)
+
+	# DESCRIPTION: Clear all the points
+	self._lineTexture.points = []
+
+	return _tmp_texture
 
 ################################################################################
 #### PUBLIC MEMBER FUNCTIONS ###################################################
@@ -39,9 +63,17 @@ func _process(_delta: float) -> void:
 	if self._drawingAllowed:
 		var _tmp_mousePosition : Vector2 = self.get_local_mouse_position()
 
-		self._cloudEmitter.position = _tmp_mousePosition
+		# DESCRIPTION: Update 
 		self._brush.position = _tmp_mousePosition
 
-		self._manage_line(self._lineVisual, _tmp_mousePosition)
-		self._manage_line(self._lineTexture, _tmp_mousePosition, false)
+		self._manage_line(self._lineVisual, _tmp_mousePosition, _lastMousePosition)
+		self._manage_line(self._lineTexture, _tmp_mousePosition, _lastMousePosition, false)
+
+		self._lastMousePosition = _tmp_mousePosition
+
+		# DESCRIPTION: Manage length of line used for texture creation. If exceeding
+		# a certain length, save the current display of the viewport into a static 
+		# texture and update the sprite's texture accordingly
+		if self._lineTexture.points.size() > 500:
+			self._mattePersistence.texture = self._save_to_texture_and_prune()
 			
